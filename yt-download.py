84164a3194cbf9fc33aee42d69845333e9f25556
Seth Taddiken <sethtaddiken@gmail.com>
2023-04-08 18:09:32 -0600

    annotation irks fixed

--- yt-download.py
+++ yt-download.py
@@ -1,16 +1,17 @@
-import yt_dlp
 import json
+
+import yt_dlp
 
 url = "https://youtu.be/caCNxVgirB0"
 
 ydl_opts = {
     "outtmpl": "music/%(title)s.%(ext)s",
     "noplaylist": True,
-    }
+}
 
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
     info = ydl.extract_info(url, download=True)
     info = ydl.sanitize_info(info)  # make info json-serializable
 
-with open(f"info/{info['title']}.json", "w") as file:
+with open(f"info/{info['title']}.json", "w") as file:  # type: ignore
     json.dump(info, file)
